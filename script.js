ef9638cd57d2081960a8ee141a52e84e8e2cfb14
haydher <70679209+haydher@users.noreply.github.com>
2021-09-09 23:48:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,275 @@
+let data = [
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/SccSCuHhOw0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBZcweVdoON6H09xKWtFSY_P-UPkg",
+  title: "Learn Express JS In 35 Minutes",
+  vidDuration: "36:03",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLQpvSjzSCSo8ZKCjBZS7TRX7omb_kyQirh2zgEY=s88-c-k-c0x00ffffff-no-rj",
+  channelName: "Web Dev Simplified",
+  views: "33K",
+  uploadTime: "1 week ago",
+  videoLink: "https://www.youtube.com/watch?v=SccSCuHhOw0",
+  channelLink: "https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/vj6rD-jvbEY/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAe0czZPllG1b_rutOtZkOJ5l1Wlw",
+  title: "Medical Emergencies: When Acts Go Horribly WRONG!",
+  vidDuration: "35:45",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLRytzj5LIQBoAeZOI-h_-CmtKQPunXKGOrJE6YTkg=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Talent Replay",
+  views: "2.3M",
+  uploadTime: "1 month ago",
+  videoLink: "https://www.youtube.com/watch?v=vj6rD-jvbEY",
+  channelLink: "https://www.youtube.com/c/talentreplay",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/t2h4XAAScb8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAdW2PFju9Yr3r3soHqWeV8-AHjuw",
+  title: "STREETBEEFS TITLE MATCH | BABY HULK vs VIKING",
+  vidDuration: "4:42",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLSHRisrv5gRSEE8Z7smC_vlWCj0ObKZ2SBJhuG3Zw=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "STREETBEEFS",
+  views: "31M",
+  uploadTime: "1 year ago",
+  videoLink: "https://www.youtube.com/watch?v=t2h4XAAScb8",
+  channelLink: "https://www.youtube.com/c/STREETBEEFSUNRELEASED",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/D7f04YOXQq8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBFprx6tCRXuYf96APDjrZZD5iYWw",
+  title: "Chainsaw Carving Competition | OT 29",
+  vidDuration: "22:11",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLSw2IlUFHy6GUZw3OPu2FZ1i5H-YQTbPX92yRS3Ig=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Dude Perfect",
+  views: "6M",
+  uploadTime: "3 days ago",
+  videoLink: "https://www.youtube.com/watch?v=D7f04YOXQq8",
+  channelLink: "https://www.youtube.com/c/DudePerfect",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/1-NxodiGPCU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCYceblt2ujD-QSc6-w7ayQX1O7vw",
+  title: "This Virus Shouldn't Exist (But it Does)",
+  vidDuration: "11:10",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLRvMf1ZTTCnC5Wc0EGOVPyrdyvfvs20vtdTUxz_vQ=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Kurzgesagt – In a Nutshell",
+  views: "7.3M",
+  uploadTime: "2 days ago",
+  videoLink: "https://www.youtube.com/watch?v=1-NxodiGPCU",
+  channelLink: "https://www.youtube.com/c/inanutshell",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/AaZ_RSt0KP8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDpEAEGwqT68j6quJNWWcltKvP69g",
+  title: "The Universe is Hostile to Computers",
+  vidDuration: "23:03",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLS6qhCOSn2LBAn1k1QnFYAMXAxGso7PnOeqWruerA=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Veritasium",
+  views: "3.4M",
+  uploadTime: "2 day ago",
+  videoLink: "https://www.youtube.com/watch?v=AaZ_RSt0KP8",
+  channelLink: "https://www.youtube.com/c/veritasium",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/19MQ1N_RGBE/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBw6tVMJbxJ4DwL2GACktR3X1Vj1w",
+  title: "The compounded tragedy of Covid-19 vaccine skeptics dying from the virus",
+  vidDuration: "4:48",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLTzs3LHHqmu-8qZiBovwo_X9sFPLdLyADWVhOglUQ=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "CNN",
+  views: "106k",
+  uploadTime: "6 hours ago",
+  videoLink: "https://www.youtube.com/watch?v=19MQ1N_RGBE",
+  channelLink: "https://www.youtube.com/user/CNN",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/SzuZnh91U8E/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA4dst_fGgRw9yQ1JT5_9iEQbZudA",
+  title: "Pawn Stars: 5 SUPER RARE ILLEGAL ITEMS | History",
+  vidDuration: "20:54",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLT6QWCoZqDkoekjMGkxPsBc0ytdOcYcrUpHUUB9=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Pawn Stars",
+  views: "24M",
+  uploadTime: "6 months ago",
+  videoLink: "https://www.youtube.com/watch?v=SzuZnh91U8E",
+  channelLink: "https://www.youtube.com/c/PawnStars",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/wqTXF6JxYhQ/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOrUTLIpbBhGZ2qnKP4a7MGEHNFw",
+  title: "Funny Local News",
+  vidDuration: "19:48",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLRC9yy7b_7INrt93uVnxfDD7-UuWmVL03OBJNOqDA=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Funny Local News",
+  views: "20M",
+  uploadTime: "4 years ago",
+  videoLink: "https://www.youtube.com/watch?v=wqTXF6JxYhQ",
+  channelLink: "https://www.youtube.com/user/FunnyLocalNews",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/S_kq5a-bu4w/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDA6EfIebQ8iqlbtTc28aDGXeoeQA",
+  title: "Young Hippo Tries to Play With Crocodile | National Geographic",
+  vidDuration: "1:29",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLTdh4vj3oGXpCXT3lMqha9_Qq4-JvMBlpztnHvi_mI=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "National Geographic",
+  views: "22M",
+  uploadTime: "4 years ago",
+  videoLink: "https://www.youtube.com/watch?v=S_kq5a-bu4w",
+  channelLink: "https://www.youtube.com/c/NatGeo",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/ooqdg9Wr-mo/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCfJtVRauH9n98QV46R3qT5OX6wfg",
+  title: "World’s Most Expensive Food!",
+  vidDuration: "8:06",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLRspawq_9jPo7BBS56l0IAeYXOFWPe5yAK4kCRReA=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Beast Reacts",
+  views: "17M",
+  uploadTime: "2 months ago",
+  videoLink: "https://www.youtube.com/watch?v=ooqdg9Wr-mo",
+  channelLink: "https://www.youtube.com/channel/UCUaT_39o1x6qWjz7K2pWcgw",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/4CzYXfBeIdM/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDlthr1YUyZca5Zb1E1acj7LXty2w",
+  title: "This Is Why Orcas Are Called Killer Whales",
+  vidDuration: "8:12",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLQ5edlr3Pn3SBaBLe9nhbn-hJVZ3k23mK4gqhhO=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "WILD VERSUS",
+  views: "39M",
+  uploadTime: "5 months ago",
+  videoLink: "https://www.youtube.com/watch?v=4CzYXfBeIdM",
+  channelLink: "https://www.youtube.com/c/WILDVERSUS",
+ },
+ {
+  thumbnail:
+   "https://i.ytimg.com/vi/n0UXA7oLc-s/hq720.jpg?sqp=-oaymwEcCNAFEJQDSEfyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD3ANw4v5sBVcDe-KoLytcuZGacBw",
+  title: "Looney Tunes Cartoons (Bugs Bunny, Daffy Duck, Porky Pig)",
+  vidDuration: "5:27:25",
+  logo: "https://yt3.ggpht.com/ytc/AKedOLQcdeTfffogmJ9Ow1Gz3rSE-qp_cCNB39F8WCwh0g=s68-c-k-c0x00ffffff-no-rj",
+  channelName: "Kartonok",
+  views: "9.1M",
+  uploadTime: "1 year ago",
+  videoLink: "https://www.youtube.com/watch?v=n0UXA7oLc-s",
+  channelLink: "https://www.youtube.com/c/TheCartoonChannel",
+ },
+ // {
+ //  thumbnail: "",
+ //  title: "",
+ //  vidDuration: "",
+ //  logo: "",
+ //  channelName: "",
+ //  views: "",
+ //  uploadTime: "",
+ //  videoLink: "",
+ //  channelLink: ""
+ // }
+];
+
+fillCard();
+
+function fillCard() {
+ const mainContent = document.querySelector(".mainContent");
+
+ for (let i = 0; i < data.length; i++) {
+  const card = document.createElement("div");
+  const cardImgContainer = document.createElement("div");
+  const videoLink = document.createElement("a");
+  const thumbnail = document.createElement("img");
+  const vidDuration = document.createElement("p");
+  const videoInfoContainer = document.createElement("div");
+  const logo = document.createElement("div");
+  const channelLogo = document.createElement("img");
+  const channelLink = document.createElement("a");
+  const videoInfo = document.createElement("div");
+  const title = document.createElement("h1");
+  const channelName = document.createElement("p");
+  const viewContainer = document.createElement("div");
+  const views = document.createElement("p");
+  const uploadTime = document.createElement("p");
+
+  card.classList.add("card");
+  cardImgContainer.classList.add("cardImgContainer");
+  vidDuration.classList.add("vidDuration");
+  videoInfoContainer.classList.add("videoInfoContainer");
+  logo.classList.add("logo");
+  videoInfo.classList.add("videoInfo");
+  title.classList.add("title");
+  channelName.classList.add("channelName");
+  viewContainer.classList.add("viewContainer");
+  views.classList.add("views");
+  uploadTime.classList.add("uploadTime");
+
+  thumbnail.src = data[i].thumbnail;
+  vidDuration.innerHTML = data[i].vidDuration;
+
+  cardImgContainer.appendChild(thumbnail);
+  cardImgContainer.appendChild(vidDuration);
+
+  videoLink.href = data[i].videoLink;
+  videoLink.appendChild(cardImgContainer);
+
+  channelLogo.src = data[i].logo;
+
+  channelLink.href = data[i].channelLink;
+  channelLink.appendChild(channelLogo);
+
+  logo.appendChild(channelLink);
+
+  title.innerHTML = data[i].title;
+  channelName.innerHTML = data[i].channelName;
+
+  views.innerHTML = `${data[i].views} views`;
+  uploadTime.innerHTML = data[i].uploadTime;
+
+  viewContainer.appendChild(views);
+  viewContainer.appendChild(uploadTime);
+
+  videoInfo.appendChild(title);
+  videoInfo.appendChild(channelName);
+  videoInfo.appendChild(viewContainer);
+
+  videoInfoContainer.appendChild(logo);
+  videoInfoContainer.appendChild(videoInfo);
+
+  card.appendChild(videoLink);
+  card.appendChild(videoInfoContainer);
+
+  mainContent.appendChild(card);
+ }
+}
+
+const menu = document.querySelector(".menu");
+const tabs = document.querySelectorAll(".tabs");
+const p = document.querySelectorAll(".tabs p");
+const asideTitle = document.querySelectorAll(".asideTitle");
+const collapsedMenu = document.querySelectorAll(".collapsedMenu");
+const aside = document.querySelector("aside");
+const divider = document.querySelectorAll(".divider");
+const footer = document.querySelector(".footer");
+const mainContent = document.querySelector(".mainContent");
+const mainNavFilter = document.querySelector(".mainNavFilter");
+
+menu.addEventListener("click", () => {
+ tabs.forEach((e) => {
+  if (!e.classList.contains("minimize")) e.classList.toggle("toggleMenu");
+ });
+
+ collapsedMenu.forEach((e) => e.classList.toggle("collapsedMenuToggle"));
+
+ p.forEach((e) => e.classList.toggle("toggleMenu"));
+
+ asideTitle.forEach((e) => e.classList.toggle("toggleMenu"));
+
+ divider.forEach((e) => e.classList.toggle("toggleMenu"));
+
+ aside.classList.toggle("asideToggleSize");
+
+ footer.classList.toggle("toggleMenu");
+
+ mainNavFilter.classList.toggle("mainNavFilterCollapsed");
+
+ mainContent.classList.toggle("mainContentMenuCollapsed");
+});
